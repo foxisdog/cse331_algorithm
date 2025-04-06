d042ebdb56fa2187bf51c356844ea9001bfe38fe
foxisdog <foxisdogmung@gmail.com>
2025-04-06 11:24:14 +0900

    빠른 알고리즘도 다시돌리자

---
+++ assignment1/testfast.sh
@@ -0,0 +1,8 @@
+ulimit -s unlimited
+./monitor ./heap ./testfiles/*.txt
+./monitor ./merge ./testfiles/*.txt
+./monitor ./tim ./testfiles/*.txt
+./monitor ./quick ./testfiles/*.txt
+./monitor ./tournament ./testfiles/*.txt
+./monitor ./intro ./testfiles/*.txt
+./monitor ./comb ./testfiles/*.txt

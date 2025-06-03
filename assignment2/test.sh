6304c789c7c4f0e8590aa9310b67343f6aac3938
foxisdog <foxisdogmung@gmail.com>
2025-06-03 22:32:11 +0900

    qwer

--- assignment2/test.sh
+++
@@ -1,6 +0,0 @@
-ulimit -v unlimited
-ulimit -s unlimited
-python3 christofides.py ./datasets/a280.csv
-python3 christofides.py ./datasets/xql662.csv
-python3 christofides.py ./datasets/kz9976.csv
-python3 christofides.py ./datasets/mona-lisa100K.csv
